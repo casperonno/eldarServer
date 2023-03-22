92226a613db8e735024c48db053652b00541178a
eldar <eli.kislan@gmail.com>
2023-03-23 00:13:55 +0200

    - Fix auto build - add aws cli and authenticate ecr commands

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Install the AWS CLI
+apt-get update && \
+apt-get install -y awscli
+
+# Authenticate with ECR
+$(aws ecr get-login --no-include-email --region ${AWS_REGION})
+
 # Build the Docker image
 docker build -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO} .
 
