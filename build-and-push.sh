71e59f5165c7d2b0ee6815ab9976ec723d2e9241
eldar <eli.kislan@gmail.com>
2023-03-22 23:53:42 +0200

    - Fix auto build - add docker file location

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build the Docker image
-docker build -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}
+docker build -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO} .
 
 # Push the Docker image to ECR
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}
